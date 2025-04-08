19bbf8e7a54db4473e7ae013252b8179de205175
louis-ab <l.abraham@students.ephec.be>
2025-04-08 15:52:01 +0200

    corrections asked

--- inginious/frontend/pages/course_admin/api_tokens.py
+++ inginious/frontend/pages/course_admin/api_tokens.py
@@ -6,9 +6,9 @@
 from collections import OrderedDict
 
 import flask
-
 import jwt
 from datetime import datetime
+from bson.objectid import ObjectId
 
 from inginious.frontend.pages.course_admin.utils import INGIniousAdminPage
 
@@ -29,18 +29,17 @@ class CourseAPITokensPage(INGIniousAdminPage):
             descr = user_input.get("descr", "")
             expire = user_input.get("expiration", "")
             if expire == "":
-                expire = datetime(9999, 12, 31)
+                expire = datetime.max
             else:
                 expire = datetime.strptime(expire, "%Y-%m-%d %H:%M:%S")
-            document = {"courseid": courseid, "expire": expire, "description": descr}
+            document = {"courseid": courseid, "exp": expire, "description": descr}
             self.database.tokens.insert_one(document)
             document["_id"] = str(document["_id"]) # otherwise can't be serialized to JSON by jwt
-            document["expire"] = str(document["expire"])
             msg = str(self.generate_token(document))
 
         else:
             tok_id = user_input.get("token_id", "")
-            self.database.tokens.delete_one({"$expr": {"$eq": ["$_id", {"$toObjectId": tok_id}]}})
+            self.database.tokens.delete_one({'_id': ObjectId(tok_id)})
             msg = "removed"
 
         return self.page(course, msg)
@@ -53,6 +52,9 @@ class CourseAPITokensPage(INGIniousAdminPage):
 
     def page(self, course, msg=""):
         """ Display the page """
-        tokens = self.database.tokens.find({"courseid": course.get_id()})
+        tokens = list(self.database.tokens.find({"courseid": course.get_id()}))
+        for token in tokens:
+            if token["exp"].year == datetime.max.year:
+                token["exp"] = "max"
         now = datetime.now()
         return self.template_helper.render("course_admin/api_tokens.html", course=course, msg=msg, tokens=tokens, now=now)
