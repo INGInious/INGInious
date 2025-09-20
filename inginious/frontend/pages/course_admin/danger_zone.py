56c3d13fdef8646211d03bdc51f1e1d473115e72
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2025-09-20 18:14:12 +0200

    [frontend] Remove use of backup directory and backup restore

--- inginious/frontend/pages/course_admin/danger_zone.py
+++ inginious/frontend/pages/course_admin/danger_zone.py
@@ -78,40 +78,6 @@ class CourseDangerZonePage(INGIniousAdminPage):
 
         self._logger.info("Course %s backed up.", courseid)
 
-    def restore_course(self, courseid, backup):
-        """ Restores a course of given courseid to a date specified in backup (format : YYYYMMDD.HHMMSS) """
-        self.wipe_course(courseid)
-
-        filepath = os.path.join(self.backup_dir, courseid, backup + ".zip")
-        with zipfile.ZipFile(filepath, "r") as zipf:
-
-            students = bson.json_util.loads(zipf.read("students.json").decode("utf-8"))
-            if len(students) > 0:
-                self.database.courses.update_one({"_id": courseid}, {"$set": {"students": students}}, upsert=True)
-
-            audiences = bson.json_util.loads(zipf.read("audiences.json").decode("utf-8"))
-            if len(audiences) > 0:
-                self.database.audiences.insert_many(audiences)
-
-            groups = bson.json_util.loads(zipf.read("groups.json").decode("utf-8"))
-            if len(groups) > 0:
-                self.database.groups.insert_many(groups)
-
-            user_tasks = bson.json_util.loads(zipf.read("user_tasks.json").decode("utf-8"))
-            if len(user_tasks) > 0:
-                self.database.user_tasks.insert_many(user_tasks)
-
-            submissions = bson.json_util.loads(zipf.read("submissions.json").decode("utf-8"))
-            for submission in submissions:
-                for key in ["input", "archive"]:
-                    if key in submission and type(submission[key]) == bson.objectid.ObjectId:
-                        submission[key] = self.submission_manager.get_gridfs().put(zipf.read(key + "/" + str(submission[key]) + ".data"))
-
-            if len(submissions) > 0:
-                self.database.submissions.insert_many(submissions)
-
-        self._logger.info("Course %s restored from backup directory.", courseid)
-
     def delete_course(self, courseid):
         """ Erase all course data """
         # Wipes the course (delete database)
@@ -120,32 +86,13 @@ class CourseDangerZonePage(INGIniousAdminPage):
         # Deletes the course from the factory (entire folder)
         self.course_factory.delete_course(courseid)
 
-        # Removes backup
-        filepath = os.path.join(self.backup_dir, courseid)
-        if os.path.exists(os.path.dirname(filepath)):
-            for backup in glob.glob(os.path.join(filepath, '*.zip')):
-                os.remove(backup)
-
         self._logger.info("Course %s files erased.", courseid)
 
     def GET_AUTH(self, courseid):  # pylint: disable=arguments-differ
         """ GET request """
         course, __ = self.get_course_and_check_rights(courseid, allow_all_staff=False)
 
-        data = flask.request.args
-
-        if "download" in data:
-            filepath = os.path.join(self.backup_dir, courseid, data["download"] + '.zip')
-
-            if not os.path.exists(os.path.dirname(filepath)):
-                raise NotFound(description=_("This file doesn't exist."))
-
-            response = Response(response=open(filepath, 'rb'), content_type='application/zip')
-            response.headers['Content-Disposition'] = 'attachment; filename="{}.zip"'.format(data["download"])
-            return response
-
-        else:
-            return self.page(course)
+        return self.page(course)
 
     def POST_AUTH(self, courseid):  # pylint: disable=arguments-differ
         """ POST request """
@@ -169,18 +116,6 @@ class CourseDangerZonePage(INGIniousAdminPage):
                 except Exception as ex:
                     msg = _("An error occurred while dumping course from database: {}").format(repr(ex))
                     error = True
-        elif "restore" in data:
-            if "backupdate" not in data:
-                msg = "No backup date selected."
-                error = True
-            else:
-                try:
-                    dt = datetime.strptime(data["backupdate"], "%Y%m%d.%H%M%S").astimezone()
-                    self.restore_course(courseid, data["backupdate"])
-                    msg = _("Course restored to date : <time datetime='{dt}'>{dt}</time>.").format(dt=dt.isoformat())
-                except Exception as ex:
-                    msg = _("An error occurred while restoring backup: {}").format(repr(ex))
-                    error = True
         elif "deleteall" in data:
             if not data.get("courseid", "") == courseid:
                 msg = _("Wrong course id.")
@@ -195,27 +130,11 @@ class CourseDangerZonePage(INGIniousAdminPage):
 
         return self.page(course, msg, error)
 
-    def get_backup_list(self, course):
-        backups = []
-
-        filepath = os.path.join(self.backup_dir, course.get_id())
-        if os.path.exists(os.path.dirname(filepath)):
-            for backup in glob.glob(os.path.join(filepath, '*.zip')):
-                try:
-                    basename = os.path.basename(backup)[0:-4]
-                    dt = datetime.strptime(basename, "%Y%m%d.%H%M%S").astimezone()
-                    backups.append({"file": basename, "date": dt})
-                except:  # Wrong format
-                    pass
-
-        return backups
 
     def page(self, course, msg="", error=False):
         """ Get all data and display the page """
         thehash = UserManager.hash_password_sha512(str(random.getrandbits(256)))
         self.user_manager.set_session_token(thehash)
 
-        backups = self.get_backup_list(course)
-
         return self.template_helper.render("course_admin/danger_zone.html", course=course, thehash=thehash,
-                                           backups=backups, msg=msg, error=error)
+                                        msg=msg, error=error)
