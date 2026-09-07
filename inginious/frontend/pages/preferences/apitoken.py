0b97dfa8f0dc16db548855a15ed46b8a7f64b4f5
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2026-09-07 14:11:49 +0200

    [api] Allow users to delete api tokens
    + small input name fix

--- inginious/frontend/pages/preferences/apitoken.py
+++ inginious/frontend/pages/preferences/apitoken.py
@@ -28,39 +28,49 @@ class APITokenPage(INGIniousAuthPage):
 
 
         user = User.objects(username=session.username).first()
-        description = request.form.get("description")
+        if "save" in request.form:
+            description = request.form.get("description")
+            expires_in = request.form.get("expires_in", 10)
 
-        expires_in = request.form.get("expires_in", 10)
-        if expires_in == "custom":
-            expires_in = int(request.form.get("custom_expiration"))
+            if expires_in == "custom":
+                expires_in = request.form.get("custom_days")
 
-        try:
-            days = int(expires_in)
-        except (TypeError, ValueError):
-            return self.show_page(errors=["Please select a valid expiration duration."])
+            try:
+                days = int(expires_in)
+            except (TypeError, ValueError):
+                return self.show_page(errors=["Please select a valid expiration duration."])
 
-        if days < 10 or days > 365:
-            return self.show_page(errors=["Expiration duration must be between 10 and 365 days."])
+            if days < 10 or days > 365:
+                return self.show_page(errors=["Expiration duration must be between 10 and 365 days."])
 
-        expiration = datetime.datetime.now(tz=timezone.utc) + datetime.timedelta(days=days)
+            expiration = datetime.datetime.now(tz=timezone.utc) + datetime.timedelta(days=days)
+            token_id = uuid.uuid4().hex
+            payload = {
+                "id": token_id,
+                "username": user.username,
+                "exp": expiration.timestamp(),
+            }
 
-        token_id = uuid.uuid4().hex
-        payload = {
-            "id": token_id,
-            "username": user.username,
-            "exp": expiration.timestamp(),
-        }
+            token = jwt.encode(payload, current_app.config["API_JWT_SECRET"], algorithm="HS256")
+            
+            try:
+                new_token = APIToken(token=UserManager.hash_password(token), expires=expiration, description=description)
+                user.apitokens[token_id] = new_token
+                user.save()
+            except ValidationError as e:
+                return self.show_page(errors=[list(e.to_dict().values())[0]])
+            return self.show_page(generated_token=token)
 
-        token = jwt.encode(payload, current_app.config["API_JWT_SECRET"], algorithm="HS256")
+        elif "delete" in request.form:
+            token_id = request.form.get("token_id")
 
-        try:
-            new_token = APIToken(token=UserManager.hash_password(token), expires=expiration, description=description)
-            user.apitokens[token_id] = new_token
+            if token_id not in user.apitokens:
+                return self.show_page(errors=["Token not found."])
+
+            del user.apitokens[token_id]
             user.save()
-        except ValidationError as e:
-            return self.show_page(errors=[list(e.to_dict().values())[0]])
-        return self.show_page(generated_token=token)
 
+            return self.show_page()
 
     def show_page(self, generated_token=None, errors=None):
         """ Prepares and shows the API token page with the list of tokens or errors. """
